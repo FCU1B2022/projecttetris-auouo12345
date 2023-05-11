3de60f37ddd2ce4fbf61ed83c5776186e1d0c124
auouo12345 <auouo12345@gmail.com>
2023-05-11 10:53:34 +0800

    WEEK 2

--- Tetris/tetris.h
+++ Tetris/tetris.h
@@ -1,3 +1,35 @@
 #include <iostream>
 
+typedef enum 
+{
+    RED = 41,
+    GREEN,
+    YELLOW,
+    BLUE,
+    PURPLE,
+    CYAN,
+    WHITE,
+    BLACK = 0,
+} Color;
+
+typedef enum 
+{
+    EMPTY = -1,
+    I,
+    J,
+    L,
+    O,
+    S,
+    T,
+    Z
+} ShapeId;
+
+typedef struct 
+{
+    ShapeId shape;
+    Color color;
+    int size;
+    char rotates[4][4][4];
+} Shape;
+
 void init();
